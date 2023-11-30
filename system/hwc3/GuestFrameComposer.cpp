a4c37e980a092f8e395ec04cc4f9ddf7515bf22d
Jason Macnak <natsu@google.com>
2023-11-29 16:00:04 -0800

    Move GuestFrameComposer's intermediate storage into per display info
    ... as upcoming changes will composition to happen in parallel across
    displays.
    
    Bug: b/248355957
    Test: vts -m VtsHalGraphicsComposer3_TargetTest
    Change-Id: Ie01cde25e89f465593a7843b51db4242d97246f2

--- system/hwc3/GuestFrameComposer.cpp
+++ system/hwc3/GuestFrameComposer.cpp
@@ -855,11 +855,12 @@ HWC3::Error GuestFrameComposer::presentDisplay(
         continue;
       }
 
-      HWC3::Error error = composeLayerInto(layer,                          //
-                                           compositionResultBufferData,    //
-                                           compositionResultBufferWidth,   //
-                                           compositionResultBufferHeight,  //
-                                           compositionResultBufferStride,  //
+      HWC3::Error error = composeLayerInto(displayInfo.compositionIntermediateStorage,  //
+                                           layer,                                       //
+                                           compositionResultBufferData,                 //
+                                           compositionResultBufferWidth,                //
+                                           compositionResultBufferHeight,               //
+                                           compositionResultBufferStride,               //
                                            4);
       if (error != HWC3::Error::None) {
         ALOGE("%s: display:%" PRIu64 " failed to compose layer:%" PRIu64,
@@ -935,6 +936,7 @@ bool GuestFrameComposer::canComposeLayer(Layer* layer) {
 }
 
 HWC3::Error GuestFrameComposer::composeLayerInto(
+    AlternatingImageStorage& compositionIntermediateStorage,
     Layer* srcLayer,                     //
     std::uint8_t* dstBuffer,             //
     std::uint32_t dstBufferWidth,        //
@@ -1012,11 +1014,10 @@ HWC3::Error GuestFrameComposer::composeLayerInto(
   // framebuffer) is one of them, so only N-1 temporary buffers are needed.
   // Vertical flip is not taken into account because it can be done together
   // with any other operation.
-  int neededScratchBuffers = (needsFill ? 1 : 0) +
-                             (needsConversion ? 1 : 0) +
-                             (needsScaling ? 1 : 0) + (needsRotation ? 1 : 0) +
-                             (needsAttenuation ? 1 : 0) +
-                             (needsBlending ? 1 : 0) + (needsCopy ? 1 : 0) - 1;
+  int neededIntermediateImages = (needsFill ? 1 : 0) + (needsConversion ? 1 : 0) +
+                                 (needsScaling ? 1 : 0) + (needsRotation ? 1 : 0) +
+                                 (needsAttenuation ? 1 : 0) + (needsBlending ? 1 : 0) +
+                                 (needsCopy ? 1 : 0) - 1;
 
   int mScratchBufferWidth =
       srcLayerDisplayFrame.right - srcLayerDisplayFrame.left;
@@ -1027,9 +1028,9 @@ HWC3::Error GuestFrameComposer::composeLayerInto(
   int mScratchBufferSizeBytes =
       mScratchBufferHeight * mScratchBufferStrideBytes;
 
-  for (int i = 0; i < neededScratchBuffers; i++) {
+  for (int i = 0; i < neededIntermediateImages; i++) {
     BufferSpec mScratchBufferspec(
-        getRotatingScratchBuffer(mScratchBufferSizeBytes, i),
+        compositionIntermediateStorage.getRotatingScratchBuffer(mScratchBufferSizeBytes, i),
         mScratchBufferWidth, mScratchBufferHeight, mScratchBufferStrideBytes);
     dstBufferStack.push_back(mScratchBufferspec);
   }
@@ -1064,7 +1065,7 @@ HWC3::Error GuestFrameComposer::composeLayerInto(
       int srcHeight = srcLayerSpec.cropHeight;
       int dst_stride_bytes =
           AlignToPower2(srcWidth * dstBufferBytesPerPixel, 4);
-      size_t needed_size = dst_stride_bytes * srcHeight;
+      size_t neededSize = dst_stride_bytes * srcHeight;
       dstBufferSpec.width = srcWidth;
       dstBufferSpec.height = srcHeight;
       // Adjust the stride accordingly
@@ -1076,7 +1077,7 @@ HWC3::Error GuestFrameComposer::composeLayerInto(
 
       // In case of a scale, the source frame may be bigger than the default tmp
       // buffer size
-      dstBufferSpec.buffer = getSpecialScratchBuffer(needed_size);
+      dstBufferSpec.buffer = compositionIntermediateStorage.getSpecialScratchBuffer(neededSize);
     }
 
     int retval = DoConversion(srcLayerSpec, dstBufferSpec, needsVFlip);
@@ -1196,28 +1197,6 @@ HWC3::Error GuestFrameComposer::applyColorTransformToRGBA(
                           bufferHeight);
 
   return HWC3::Error::None;
-}
-
-uint8_t* GuestFrameComposer::getRotatingScratchBuffer(std::size_t neededSize,
-                                                      std::uint32_t order) {
-  static constexpr const int kNumScratchBufferPieces = 2;
-
-  std::size_t totalNeededSize = neededSize * kNumScratchBufferPieces;
-  if (mScratchBuffer.size() < totalNeededSize) {
-    mScratchBuffer.resize(totalNeededSize);
-  }
-
-  std::size_t bufferIndex = order % kNumScratchBufferPieces;
-  std::size_t bufferOffset = bufferIndex * neededSize;
-  return &mScratchBuffer[bufferOffset];
-}
-
-uint8_t* GuestFrameComposer::getSpecialScratchBuffer(size_t neededSize) {
-  if (mSpecialScratchBuffer.size() < neededSize) {
-    mSpecialScratchBuffer.resize(neededSize);
-  }
-
-  return &mSpecialScratchBuffer[0];
 }
 
 }  // namespace aidl::android::hardware::graphics::composer3::impl
